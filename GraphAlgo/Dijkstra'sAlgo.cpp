fccd61171d17657a9781eef692dfb1b5d00d2e7b
HRKagdi <53858937+HRKagdi@users.noreply.github.com>
2022-05-28 16:05:38 +0530

    Create Dijkstra'sAlgo.cpp

---
+++ GraphAlgo/Dijkstra'sAlgo.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+int minDistance(vector<int>&dist,vector<bool>&vis){
+    int minDist=INT_MAX; int ind=0;
+    for(int i=0;i<dist.size();i++){
+        if(!vis[i] && minDist>dist[i]){
+            minDist=dist[i];
+            ind=i;
+        }
+    }
+    return ind;
+}
+int main(){
+    int t; cin>>t;
+    while(t--){
+        int n,e; cin>>n>>e;
+        vector<pair<int,pair<int,int>>> g;
+        for(int i=1;i<=e;i++){
+            int a,b,w; cin>>a>>b>>w;
+            g.push_back(make_pair(w,make_pair(a,b)));
+            g.push_back(make_pair(w,make_pair(b,a)));
+
+        }
+        vector<bool> vis(n,false); vector<int> dis(n,INT_MAX);
+    	dis[0]=0;
+        
+        // Dijkstra's ALgo.
+    	for(int i=0;i<e;i++){
+            int m=minDistance(dis,vis);
+            vis[m]=true;
+            for(int i=0;i<g.size();i++){
+                if(g[i].second.first==m){
+                    dis[g[i].second.second]=min(dis[g[i].second.second], dis[m]+g[i].first);
+                }
+            }
+        }
+        for(int i=0;i<n;i++) cout<<i<<" "<<dis[i]<<endl;
+    }
+    
+    return 0;
+}
