bd36fee5446c3cf4610f58bccee3826293eb6375
HRKagdi <53858937+HRKagdi@users.noreply.github.com>
2022-05-29 16:38:13 +0530

    Create Kadanes.cpp

---
+++ Arrays/Kadanes.cpp
@@ -0,0 +1,13 @@
+//https://leetcode.com/problems/maximum-subarray/submissions/
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int cs,gs; cs=gs=nums[0];
+        for(int i=1;i<nums.size();i++){
+            cs=max(nums[i],cs+nums[i]);
+            if(cs>gs) gs=cs;
+        }
+        return gs;
+    }
+};
